af88b4b1d21a2b7965574ee4c389645cae6f346b
rudra0133 <151999862+rudra0133@users.noreply.github.com>
2023-11-28 10:18:38 +0000

    Introductionfile saved

---
+++ 01-basics/test.js
@@ -0,0 +1,8 @@
+/* Introduction:- 
+1.) JavaScript is a High level scripting language use on developing web and mobile application efficiently.
+2.) We used javascript on client side as well aas server side.
+3.) In Javascript we do logical implementation in webpages.
+4.) In today era we used Es6 version of javascript,as it is largest one.
+* Es6-Echma Script 6 (vanilla javascript) 
+*/
+console.log("Namaste Dunia");
